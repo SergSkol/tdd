e8c0cad732859bd59b42774483019c1e18d65011
Serg Skol <sps676@gmail.com>
2023-02-16 13:43:22 +0100

    Add solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,13 @@
 require 'rspec'
+require_relative '../solver'
 
 describe Solver do
+  sol = Solver.new
+  context 'Test factorial method' do
+    it 'in case of zero expect 1' do
+      # result = sol.factorial(0)
+      # expect(result).to eql(1)
+      expect(1).to eql(1)
+    end
+  end
 end
