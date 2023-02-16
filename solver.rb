1f975b2cdcd2e48ef0fc2462d955260bf8071db4
Nabeel Akbar <studiosnabeel@gmail.com>
2023-02-16 18:12:17 +0500

    Add fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -20,4 +20,14 @@ class Solver
     end
     result
   end
+
+  def fizzbuzz(number)
+    return 'fizzbuzz' if (number % 3).zero? && (number % 5).zero?
+
+    return 'fizz' if (number % 3).zero?
+
+    return 'buzz' if (number % 5).zero?
+
+    number
+  end
 end
